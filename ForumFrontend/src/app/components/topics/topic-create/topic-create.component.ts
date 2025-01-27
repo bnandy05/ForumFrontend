838ca1774f548f09b28ce21f251a7d3ca411f999
bandiiskola <98553047+bnandy05@users.noreply.github.com>
2025-01-27 11:04:37 +0100

    Initialization

---
+++ ForumFrontend/src/app/components/topics/topic-create/topic-create.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-topic-create',
+  imports: [],
+  templateUrl: './topic-create.component.html',
+  styleUrl: './topic-create.component.css'
+})
+export class TopicCreateComponent {
+
+}
---
+++ ForumFrontend/src/app/components/topics/topic-details/topic-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-topic-details',
+  imports: [],
+  templateUrl: './topic-details.component.html',
+  styleUrl: './topic-details.component.css'
+})
+export class TopicDetailsComponent {
+
+}
