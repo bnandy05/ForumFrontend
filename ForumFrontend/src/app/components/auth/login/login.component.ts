4f157676679d532fcbfff942704126e248967273
bnandy05 <98553047+bnandy05@users.noreply.github.com>
2025-02-05 18:46:11 +0100

    Build

--- ForumFrontend/src/app/components/auth/login/login.component.ts
+++ ForumFrontend/src/app/components/auth/login/login.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrl: './login.component.css'
 })
 export class LoginComponent {
   email: string = '';
