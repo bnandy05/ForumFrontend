5bcb6a9876e173c9b33bb37f954688f8df89a3d3
bnandy05 <98553047+bnandy05@users.noreply.github.com>
2025-02-13 18:18:43 +0100

    login tisztitás

--- ForumFrontend/src/app/components/auth/login/login.component.ts
+++ ForumFrontend/src/app/components/auth/login/login.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
-import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';  // Import HttpClient
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
@@ -15,28 +13,12 @@ import { CommonModule } from '@angular/common';
 export class LoginComponent {
   email: string = '';
   password: string = '';
-  errorMessage: string = '';
 
   constructor(
-    private authService: AuthService, 
-    private router: Router, 
-    private http: HttpClient  // Inject HttpClient
+    private authService: AuthService
   ) {}
 
   onSubmit() {
-    const loginData = { email: this.email, password: this.password };
-  
-    this.http.post('https://berenandor.moriczcloud.hu/api/login', loginData, { withCredentials: true }).subscribe(
-      (response: any) => {
-        // Sikeres bejelentkezés
-        localStorage.setItem('token', response.token);
-        alert('Sikeres bejelentkezés!');
-        this.router.navigate(['/dashboard']);
-      },
-      (error) => {
-        // Hiba kezelése
-        this.errorMessage = 'Hibás bejelentkezési adatok. Kérlek, próbáld újra.';
-      }
-    );
+    this.authService.login(this.email, this.password);
   }
 }
