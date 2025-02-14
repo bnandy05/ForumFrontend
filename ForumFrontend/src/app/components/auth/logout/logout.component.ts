31e48cf7533c1200e6ce40a8992f268f637d42d9
bandiiskola <98553047+bnandy05@users.noreply.github.com>
2025-02-14 09:24:55 +0100

    logout

---
+++ ForumFrontend/src/app/components/auth/logout/logout.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-logout',
+  imports: [],
+  templateUrl: './logout.component.html',
+  styleUrl: './logout.component.css'
+})
+export class LogoutComponent implements OnInit{
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
