4be004afe8bddfcd8b7c9b2ac0e839221a04ffb1
bnandy05 <98553047+bnandy05@users.noreply.github.com>
2025-02-14 17:02:11 +0100

    időzóna bug

--- ForumFrontend/src/app/components/home/home.component.ts
+++ ForumFrontend/src/app/components/home/home.component.ts
@@ -1,36 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import { HeaderComponent } from '../header/header.component';
 import { TopicService } from '../../services/topic.service';
+import { HeaderComponent } from '../header/header.component';
 import { CommonModule } from '@angular/common';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc'; // UTC plugin
+import relativeTime from 'dayjs/plugin/relativeTime';
+import 'dayjs/locale/hu';
 
-export interface Topic {
-  id: number;
-  title: string;
-  content: string;
-  user: {
-    id: number;
-    name: string;
-  };
-  created_at: string;
-}
-
+dayjs.extend(utc); // UTC plugin
+dayjs.extend(relativeTime);
+dayjs.locale('hu');
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   imports: [HeaderComponent, CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit{
-  topics: Topic[] = [];
+export class HomeComponent implements OnInit {
+  topics: any[] = [];
 
   constructor(private topicService: TopicService) {}
 
   ngOnInit() {
+    console.log('Current date:', dayjs().format());
     this.topicService.getTopics().subscribe({
-      next: (topics) => {
-        console.log('Topics:', topics);
-        this.topics = topics.data;
+      next: (response) => {
+        console.log('Topics:', response.data);
+        this.topics = response.data.map((topic: any) => ({
+          ...topic,
+          timeAgo: dayjs.utc(topic.created_at).local().fromNow() // UTC-ről helyi időzónára, majd relatív idő
+        }));
       },
       error: (err) => {
         console.error('Failed to fetch topics:', err);
