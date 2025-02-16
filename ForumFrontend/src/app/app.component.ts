eed429ca581efd1cbdbfae095fb59277367a7f92
bnandy05 <98553047+bnandy05@users.noreply.github.com>
2025-02-16 23:00:42 +0100

    no style toast

--- ForumFrontend/src/app/app.component.ts
+++ ForumFrontend/src/app/app.component.ts
@@ -3,10 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { authInterceptor } from './auth.interceptor';
 import { CommonModule } from '@angular/common';
+import { MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
+import { ButtonModule } from 'primeng/button';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, ToastModule, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -14,9 +18,17 @@ export class AppComponent {
   title = 'ForumFrontend';
   isLoading = true;
 
-  constructor() {
+  constructor(private messageService: MessageService) {
     setTimeout(() => {
       this.isLoading = false;
-    }, 300);
+    }, 300)
+  }
+
+  showSuccess() {
+    this.messageService.add({ severity: 'success', summary: 'Siker!', detail: 'Ez egy sikeres üzenet' });
+  }
+
+  showError() {
+    this.messageService.add({ severity: 'error', summary: 'Hiba!', detail: 'Ez egy hibaüzenet' });
   }
 }
--- ForumFrontend/src/main.ts
+++ ForumFrontend/src/main.ts
@@ -6,10 +6,16 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './app/auth.interceptor';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { MessageService } from 'primeng/api';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { ToastModule } from 'primeng/toast';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor]))
+    provideHttpClient(withInterceptors([authInterceptor])),
+    importProvidersFrom(ToastModule),
+    provideAnimations(),
+    MessageService
   ],
 }).catch(err => console.error(err));
