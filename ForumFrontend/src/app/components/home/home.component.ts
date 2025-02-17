c14ac0ace8cb1fb939989de9cc5955c84dbc9866
bandiiskola <98553047+bnandy05@users.noreply.github.com>
2025-02-17 12:52:27 +0100

    szűrés

--- ForumFrontend/src/app/components/home/home.component.ts
+++ ForumFrontend/src/app/components/home/home.component.ts
@@ -3,6 +3,7 @@ import { TopicService } from '../../services/topic.service';
 import { HeaderComponent } from '../header/header.component';
 import { CommonModule } from '@angular/common';
 import { SafeHtmlPipe } from '../../safe-html.pipe';
+import { FormsModule } from '@angular/forms';
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
 import relativeTime from 'dayjs/plugin/relativeTime';
@@ -15,17 +16,21 @@ dayjs.locale('hu');
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, CommonModule, SafeHtmlPipe],
+  imports: [HeaderComponent, CommonModule, SafeHtmlPipe, FormsModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
   topics: any[] = [];
+  title: string = "";
+  orderBy: any = "";
+  categoryId: number = 0
+  categories: any[] = [];
 
   constructor(private topicService: TopicService) {}
 
-  ngOnInit() {
-    this.topicService.getTopics().subscribe({
+  filterTopics() {
+    this.topicService.getTopics(this.categoryId, this.title, this.orderBy).subscribe({
       next: (response) => {
         this.topics = response.data.map((topic: any) => ({
           ...topic,
@@ -35,6 +40,18 @@ export class HomeComponent implements OnInit {
       error: (err) => {
         console.error('Failed to fetch topics:', err);
       }
+    });
+  }
+
+  ngOnInit() {
+    this.filterTopics();
+    this.topicService.getCategories().subscribe({
+      next: (response) => {
+        this.categories = response;
+      },
+      error: (err) => {
+        console.error('Failed to fetch categories:', err);
+      },
     });
   }
 }
