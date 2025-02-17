ae89a8a6d70c38013a621046779cd4732e8d6e6c
Meszarosistvan0828 <84836386+Meszarosistvan0828@users.noreply.github.com>
2025-02-17 12:41:04 +0100

    xd

--- ForumFrontend/src/app/services/user.service.ts
+++ ForumFrontend/src/app/services/user.service.ts
@@ -4,6 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class UserService {
+  private apiUrl = 'https://berenandor.moriczcloud.hu/api/forum';
+
 
   constructor() { }
 }
