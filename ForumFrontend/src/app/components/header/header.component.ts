38d946c89303b6faadfc33bbd8f8d0c3aa169f61
bandiiskola <98553047+bnandy05@users.noreply.github.com>
2025-02-17 09:12:23 +0100

    logout hiba megoldás

--- ForumFrontend/src/app/components/header/header.component.ts
+++ ForumFrontend/src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -10,7 +11,9 @@ import { AuthService } from '../../services/auth.service';
 export class HeaderComponent implements OnInit{
   username: string = '';
 
-  constructor(private authService: AuthService)
+  constructor(private authService: AuthService,
+    private router: Router
+  )
   {}
 
   ngOnInit() {
@@ -21,6 +24,7 @@ export class HeaderComponent implements OnInit{
       (error) => {
         console.error('Error fetching user data:', error);
         this.username = "";
+        this.router.navigate(['/logout']);
       }
     );
   }
