cfd1a72f4f153f4918830c1c6914d5d131bfc984
bandiiskola <98553047+bnandy05@users.noreply.github.com>
2025-02-17 10:04:16 +0100

    jobb header

--- ForumFrontend/src/app/components/header/header.component.ts
+++ ForumFrontend/src/app/components/header/header.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import { MenubarModule } from 'primeng/menubar';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MenubarModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent implements OnInit{
   username: string = '';
+  items: any[] = [
+    {
+      label: 'Kezdőlap',
+      icon: 'pi pi-home',
+      routerLink: ['/']
+    },
+    {
+        label: 'Topic Feltöltés',
+        icon: 'pi pi-laptop',
+        routerLink: ['/create'] 
+    },
+    {
+        label: 'Én Topicjaim',
+        icon: 'pi pi-mobile',
+        routerLink: ['/My-topics']
+
+    }
+  ];
+  secondaryitems: any[] = [
+    {
+      label: 'Profil',
+      icon: 'pi pi-info-circle',
+      routerLink: ['/profile']
+    },
+    {
+      label: 'Kijelentkezés',
+      icon: 'pi pi-info-circle',
+      routerLink: ['/logout']
+    }
+  ]
 
   constructor(private authService: AuthService,
     private router: Router
