9b97dc9e08f35806107965f850c607f1c72481fd
Meszarosistvan0828 <meszarosistvan0828@gmail.com>
2025-02-25 21:29:43 +0100

    haladunk

--- ForumFrontend/src/app/components/theme-toggle/theme-toggle.component.ts
+++ ForumFrontend/src/app/components/theme-toggle/theme-toggle.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { ThemeService } from '../../services/theme.service';
+import { PrimeIcons } from 'primeng/api';
 
 @Component({
   selector: 'app-theme-toggle',
