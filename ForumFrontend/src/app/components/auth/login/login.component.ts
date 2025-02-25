588ce0ab163e014f17e7c5d25dfc895b78b2e99c
Meszarosistvan0828 <meszarosistvan0828@gmail.com>
2025-02-25 23:36:03 +0100

    Update login.component.ts

--- ForumFrontend/src/app/components/auth/login/login.component.ts
+++ ForumFrontend/src/app/components/auth/login/login.component.ts
@@ -2,12 +2,10 @@ import { Component } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { ThemeToggleComponent } from '../../theme-toggle/theme-toggle.component';
-
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule, ThemeToggleComponent],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
