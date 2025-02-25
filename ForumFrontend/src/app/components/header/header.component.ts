32b8e2ca5b6cb92ae741f2801dbef1cd0e7bcadf
Meszarosistvan0828 <meszarosistvan0828@gmail.com>
2025-02-25 21:23:16 +0100

    almafa

--- ForumFrontend/src/app/components/header/header.component.ts
+++ ForumFrontend/src/app/components/header/header.component.ts
@@ -4,10 +4,11 @@ import { Router } from '@angular/router';
 import { AvatarModule } from 'primeng/avatar';
 import { AvatarGroupModule } from 'primeng/avatargroup';
 import { PrimeIcons } from 'primeng/api';
+import { ThemeToggleComponent } from '../theme-toggle/theme-toggle.component';
 
 @Component({
   selector: 'app-header',
-  imports: [AvatarModule, AvatarGroupModule],
+  imports: [AvatarModule, AvatarGroupModule,ThemeToggleComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
