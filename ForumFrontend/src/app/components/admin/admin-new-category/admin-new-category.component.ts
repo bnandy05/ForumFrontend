c23983b2a773ac63b059e375c1bbdd337e259759
Meszarosistvan0828 <84836386+Meszarosistvan0828@users.noreply.github.com>
2025-03-05 10:29:11 +0100

    commit

--- ForumFrontend/src/app/components/admin/admin-new-category/admin-new-category.component.ts
+++ ForumFrontend/src/app/components/admin/admin-new-category/admin-new-category.component.ts
@@ -1,8 +1,9 @@
+import { HeaderComponent } from './../../header/header.component';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-admin-new-category',
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './admin-new-category.component.html',
   styleUrl: './admin-new-category.component.css'
 })
