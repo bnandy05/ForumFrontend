20bc59867f074d29fd4912e960f71a3f60cf2bf5
bandiiskola <98553047+bnandy05@users.noreply.github.com>
2025-03-05 13:46:44 +0100

    admin működés

--- ForumFrontend/src/app/components/admin/admin-table/admin-table.component.ts
+++ ForumFrontend/src/app/components/admin/admin-table/admin-table.component.ts
@@ -1,19 +1,77 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from '../../header/header.component';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AdminService } from '../../../services/admin.service';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../../header/header.component';
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+interface UsersResponse {
+  data: User[];
+  current_page: number;
+  last_page: number;
+}
 
 @Component({
   selector: 'app-admin-table',
-  imports: [HeaderComponent],
   templateUrl: './admin-table.component.html',
-  styleUrl: './admin-table.component.css'
+  standalone: true,
+  imports: [CommonModule, HeaderComponent],
+  styleUrls: ['./admin-table.component.css']
 })
-export class AdminTableComponent {
+export class AdminTableComponent implements OnInit {
+  users: UsersResponse = {
+    data: [],
+    current_page: 0,
+    last_page: 0
+  };
+  loading = false;
+  allLoaded = false;
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router, 
+    private adminService: AdminService
+  ) {}
 
-  userClick(userId: number, event: MouseEvent): void {
+  ngOnInit(): void {
+    this.loadMore();
+  }
+
+  userClick(userId: number): void {
     this.router.navigate(['/profile', userId]);
   }
 
+  loadMore(): void {
+    if (this.loading || this.allLoaded) return;
+
+    this.loading = true;
+
+    const nextPage = this.users.current_page + 1;
+
+    this.adminService.getUsers(nextPage).subscribe({
+      next: (response) => {
+        this.users.data = [
+          ...this.users.data, 
+          ...response.users.data
+        ];
+
+        this.users.current_page = response.users.current_page;
+        this.users.last_page = response.users.last_page;
+
+        if (this.users.current_page >= this.users.last_page) {
+          this.allLoaded = true;
+        }
+
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error('Felhasználók betöltése sikertelen:', err);
+        this.loading = false;
+      }
+    });
+  }
 }
--- ForumFrontend/src/app/services/admin.service.ts
+++ ForumFrontend/src/app/services/admin.service.ts
@@ -1,33 +1,38 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
+import { MessageService } from 'primeng/api';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
 
-  private baseUrl = 'https://berenandor.moriczcloud.hu/api';
+  private baseUrl = 'https://berenandor.moriczcloud.hu/api/forum';
 
   constructor(private http: HttpClient) {}
 
   banUser(id: number): Observable<any> {
-    return this.http.post(`${this.baseUrl}/forum/admin/user/ban`, { body: { id } });
+    return this.http.post(`${this.baseUrl}/admin/user/ban`, { body: { id } } ,{withCredentials: true});
   }
 
   deleteUser(id: number): Observable<any> {
-    return this.http.delete(`${this.baseUrl}/forum/admin/user/delete`, { body: { id } });
+    return this.http.delete(`${this.baseUrl}/admin/user/delete`, { body: { id } });
   }
 
   deleteTopic(id: number): Observable<any> {
-    return this.http.delete(`${this.baseUrl}/forum/admin/topic/delete`, { body: { id } });
+    return this.http.delete(`${this.baseUrl}/admin/topic/delete`, { body: { id } });
   }
 
   deleteComment(id: number): Observable<any> {
-    return this.http.delete(`${this.baseUrl}/forum/admin/comment/delete`, { body: { id } });
+    return this.http.delete(`${this.baseUrl}/admin/comment/delete`, { body: { id } });
   }
 
   uploadCategory(name: string): Observable<any> {
-    return this.http.post(`${this.baseUrl}/forum/admin/category/upload`, { name });
+    return this.http.post(`${this.baseUrl}/admin/category/upload`, { name }, {withCredentials: true});
+  }
+
+  getUsers(page: number =1): Observable<any> {
+    return this.http.get(`${this.baseUrl}/admin/users/get?page=${page}` ,{withCredentials: true});
   }
 }
