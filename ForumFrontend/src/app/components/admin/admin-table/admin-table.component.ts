c70157708e2aac1e9c5cc1eddc082b8af393eadf
Meszarosistvan0828 <84836386+Meszarosistvan0828@users.noreply.github.com>
2025-03-05 10:17:10 +0100

    filter

--- ForumFrontend/src/app/components/admin/admin-table/admin-table.component.ts
+++ ForumFrontend/src/app/components/admin/admin-table/admin-table.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../header/header.component';
-HeaderComponent
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-admin-table',
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './admin-table.component.html',
   styleUrl: './admin-table.component.css'
 })
 export class AdminTableComponent {
+
+  constructor(private router: Router) {}
+
+  userClick(userId: number, event: MouseEvent): void {
+    this.router.navigate(['/profile', userId]);
+  }
 
 }
